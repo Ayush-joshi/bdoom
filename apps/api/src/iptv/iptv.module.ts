595085d7c618dbed3a4221597214aa28606e3cf4
Ayush Joshi <ayushjoshi@Ayushs-MacBook-Air.local>
2026-07-11 01:39:41 +0530

    Fix IPTV module auth dependency

--- apps/api/src/iptv/iptv.module.ts
+++ apps/api/src/iptv/iptv.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { AuthModule } from '../auth/auth.module';
 import { IptvController } from './iptv.controller';
 
 @Module({
+  imports: [AuthModule],
   controllers: [IptvController],
 })
 export class IptvModule {}
